9e319b8b8c7a1fd41ac0fae7389895052deca753
Duong Dang <duongplus1011@gmail.com>
2025-04-20 14:10:19 +0700

    add-> user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  phone: string;
+  
+  @Prop({ required: true })
+  age: number;
+
+  @Prop({ required: true })
+  address: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
